d7326927e9117bbd044d2a45c59802370a24176d
Lj1210 <liujing2592@gmail.com>
2021-07-19 22:25:45 +0800

    修改了ObjectTest2中Person类的名称，解决了ObjectTest3中不报错但是运行不出来的问题

--- Day07/src/ObjectTest2.java
+++ Day07/src/ObjectTest2.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class ObjectTest2 {
+class Test {
+    private int age;
+
+    public void setAge(int a) {
+        if (a > 0 && a < 120) {
+            age = a;
+            speak();
+        } else
+            System.out.print("no");
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    void speak() {
+        System.out.print("age=" + age);
+    }
+}
+
+public class ObjectTest2 {
+    public static void main(String[] args) {
+        Test p = new Test();
+        p.setAge(-40);
+    }
 }
