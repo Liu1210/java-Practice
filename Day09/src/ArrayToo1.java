023bcc1245304aa7a732cc8be9b6eec20fe44fcd
Lj1210 <liujing2592@gmail.com>
2021-07-22 10:57:04 +0800

    静态的应用-工具类练习代码

---
+++ Day09/src/ArrayToo1.java
@@ -0,0 +1,15 @@
+public class ArrayToo1Demo {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 87, 32, 8};
+        ArrayToo1 tool = new ArrayToo1();
+        int max = too1.getMax(arr);
+        System.out.println("max=" + min);
+        too1.printArray(arr);
+        System.out.println("min+" + min);
+        too1.printArray(arr);
+        too1.selectSort(arr);
+        too1.printArray(arr);
+        int[] arr1 = ();
+        ArrayToo1 too11 = new ArrayToo1();
+    }
+}
