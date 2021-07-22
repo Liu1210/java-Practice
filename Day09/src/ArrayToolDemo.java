22f3bf647a2789702c705d4214206eb8f7de9ffa
Lj1210 <liujing2592@gmail.com>
2021-07-22 17:20:04 +0800

    修改了部分错误，新写了静态代码块练习代码

---
+++ Day09/src/ArrayToolDemo.java
@@ -0,0 +1,18 @@
+public class ArrayToo1Demo {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 87, 5, 12};
+        int max = ArrayToo1.getMax(arr);
+        System.out.println("max=" + max);
+        /**
+         ArrayToo1 too1 = new ArrayToo1();
+         int max = too1.getMax(arr);
+         System.out.println("max=" + max);
+
+         int min = too1.getMax(arr);
+         System.out.println("min=" + min);
+         too1.printArray(arr);
+         too1.selectSort(arr);
+         too1.printArray(arr);
+         */
+    }
+}
