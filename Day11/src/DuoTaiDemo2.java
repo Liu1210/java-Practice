eff5f89f7a5c7cd2e8e2e5cde775c122a945dd52
Lj1210 <liujing2592@gmail.com>
2021-07-24 20:31:42 +0800

    多态-示例练习代码

--- Day11/src/DuoTaiDemo2.java
+++ Day11/src/DuoTaiDemo2.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class DuoTaiDemo2 {
+abstract class Student1 {
+    public abstract void study();
+
+    public void sleep() {
+        System.out.println("躺着睡");
+    }
+}
+
+class BaseStudent extends Student1 {
+    public void study() {
+        System.out.println("base study");
+    }
+
+    public void sleep() {
+        System.out.println("趴着睡");
+    }
+}
+
+class AdvStudent extends Student1 {
+    public void study() {
+        System.out.println("adv study");
+    }
+}
+
+
+public class DuoTaiDemo2 {
+    public static void main(String[] args) {
+        DoStudent ds = new DoStudent();
+        ds.doSome(new BaseStudent());
+        ds.doSome(new AdvStudent());
+    }
 }
