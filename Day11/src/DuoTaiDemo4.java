26546594e9f643bffa520b4676e1ac561a836134
Lj1210 <liujing2592@gmail.com>
2021-07-24 21:58:03 +0800

    多态的主板示例练习代码

--- Day11/src/DuoTaiDemo4.java
+++ Day11/src/DuoTaiDemo4.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class DuoTaiDemo4 {
+interface PCI {
+    public void open();
+
+    public void close();
+}
+
+class MainBoard {
+    public void run() {
+        System.out.println("mainboard run");
+    }
+
+    public void usePCI(PCI P) {
+        if (P != null) {
+            P.open();
+            P.close();
+        }
+    }
+}
+
+class NetCard implements PCI {
+    public void open() {
+        System.out.println("netcard open");
+    }
+
+    public void close() {
+        System.out.println("netcard close");
+    }
+}
+
+class SoundCard implements PCI {
+    public void open() {
+        System.out.println("SoundCard open");
+    }
+
+    public void close() {
+        System.out.println("SoundCard close");
+    }
+}
+
+public class DuoTaiDemo4 {
+    public static void main(String[] args) {
+        MainBoard mb = new MainBoard();
+        mb.run();
+        mb.usePCI(null);
+        mb.usePCI(new NetCard());
+        mb.usePCI(new SoundCard());
+    }
 }
