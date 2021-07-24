ef4c33cf0a92d977a775c623f5d3fe2ff5c4d5c9
Lj1210 <liujing2592@gmail.com>
2021-07-24 21:14:21 +0800

    多态中成员的特点练习代码

--- Day11/src/DouTaiDemo3.java
+++ Day11/src/DouTaiDemo3.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class DouTaiDemo3 {
+class Fu {
+    int num = 5;
+
+    void method1() {
+        System.out.println("fu method_1");
+    }
+
+    void method2() {
+        System.out.println("fu method_2");
+    }
+
+    static void method4() {
+        System.out.println("fu method_4");
+    }
+}
+
+class Zi extends Fu {
+    int num = 8;
+
+    void method1() {
+        System.out.println("zi method_1");
+    }
+
+    void method3() {
+        System.out.println("zi method_3");
+    }
+
+    static void method4() {
+        System.out.println("fu method_4");
+    }
+}
+
+public class DouTaiDemo3 {
+    public static void main(String[] args) {
+        Fu f = new Zi();
+        System.out.println(f.num);
+        f.method4();
+        Zi z = new Zi();
+        z.method4();
+    }
+
 }
