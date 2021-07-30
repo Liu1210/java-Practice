9a17b5df867de7d8c12bedc22309e66456acbed8
Lj1210 <liujing2592@gmail.com>
2021-07-30 22:38:05 +0800

    同步函数，单例设计，死锁，线程间通信练习代码

---
+++ Day16/src/DeadLockTest.java
@@ -0,0 +1,38 @@
+class Test implements Runnable {
+    private boolean flag;
+
+    Test(boolean flag) {
+        this.flag = flag;
+    }
+
+    public void run() {
+        if (flag) {
+            synchronized (MyLock.locka) {
+                System.out.println("if lockb");
+                synchronized (MyLock.lockb) {
+                    System.out.println("if locka");
+                }
+            }
+        }
+        else {
+            synchronized (MyLock.lockb) {
+                System.out.println("else lockb");
+                synchronized (MyLock.locka) {
+                    System.out.println("else locka");
+                }
+            }
+        }
+    }
+}
+class MyLock{
+    static Object locka= new Object();
+    static Object lockb= new Object();
+}
+class DeadLockTest {
+    public static void main(String[] args){
+        Thread t1 =new Thread(new Test(true));
+        Thread t2 =new Thread(new Test(false));
+        t1.start();
+        t2.start();
+    }
+}
---
+++ Day16/src/SingleDemo.java
@@ -0,0 +1,19 @@
+class Single {
+    private static Single s = null;
+
+    private Single() {
+    }
+
+    public static Single getInstance() {
+        if (s == null) {
+            synchronized (Single.class) {
+                if (s == null)
+                    s = new Single();
+            }
+        }
+        return s;
+    }
+
+    public class SingleDemo {
+    }
+}
