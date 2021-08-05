42e135681fd4fc18d7d75a81ffa1b86e4c5c7e7e
Lj1210 <liujing2592@gmail.com>
2021-08-05 12:49:51 +0800

    test：测试

--- Day05/src/ArrayTest2.java
+++ Day05/src/ArrayTest2.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ArrayTest2 {
+public class ArrayTest2 {
+    public static void main(String[] srgs) {
+        int[] arr = {2, 5, 7, 9, 8};
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int x = 0; x < arr.length; x++) {
+            if (x != arr.length - 1)
+                System.out.print(arr[x] + ",");
+            else
+                System.out.print(arr[x]);
+        }
+    }
 }
